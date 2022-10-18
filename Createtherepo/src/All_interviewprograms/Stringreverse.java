33bc8b21008a5a22b9f38407625f092e03845060
gitlabtestingprabhu <gitlabtesting2022@gmail.com>
2022-10-18 15:08:22 +0530

    tsts

---
+++ Createtherepo/src/All_interviewprograms/Stringreverse.java
@@ -0,0 +1,23 @@
+package All_interviewprograms;
+
+import java.util.Iterator;
+
+public class Stringreverse {
+
+	public static void main(String[] args) {
+		
+		String s="javajava";
+		String r = null;
+		
+		for (int i =s.length()-1; i > 0; i--) {
+			
+		char c = s.charAt(i);
+			
+		
+		r=r+c;
+		System.out.println(r);
+		}
+		
+		
+	}
+}
